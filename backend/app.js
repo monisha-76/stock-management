ef3fe16d2b1f056a9e3a28c4b065f0bae511ef4a
monika2416090 <mithunm.it2023@citchennai.net>
2025-06-17 11:28:56 +0530

    13th commit

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,8 @@ app.use(cors({
 }));
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: "200mb" }));
+app.use(express.urlencoded({ extended: true, limit: "200mb" }));
 
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
